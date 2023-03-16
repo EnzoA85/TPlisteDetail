85c810d3faf81790dcbcbc8ce482f27f2d107d29
Enzo ARCHAMBAUD <bib.enzoarchambaud@gmail.com>
2023-03-16 15:42:46 +0100

    add favorite button function but favorite button staid on for all serie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router,ActivatedRoute } from '@angular/router';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-detail',
@@ -11,23 +12,38 @@ export class DetailPage implements OnInit {
   item;
   public imageFavori = "./assets/étoileVide.png";
 
-  constructor(private router: Router, private activeRoute : ActivatedRoute) {
+  constructor(private storage: Storage,private router: Router, private activeRoute : ActivatedRoute) {
     this.activeRoute.queryParams.subscribe(params=>{
       if(this.router.getCurrentNavigation().extras.state){
         this.item=this.router.getCurrentNavigation().extras.state.param1;
       }
     })
+
+    this.storage.get('favori')
+      .then(
+        (val)=>{
+          if (val != null) {
+            this.imageFavori = './assets/étoilePleine.png';
+            console.log(val);
+          } else {
+            console.log('vide');
+          }
+        }
+      );
+    
    }
 
   ngOnInit() {
   }
 
-  addFavorie()
+  addFavorie(item)
   {
     if (this.imageFavori=='./assets/étoileVide.png') {
   		this.imageFavori='./assets/étoilePleine.png';
+      this.storage.set('favori',item);
   	} else {
-		this.imageFavori = './assets/étoileVide.png';
+		  this.imageFavori = './assets/étoileVide.png';
+      this.storage.remove('favori');
   	}
   }
 }
